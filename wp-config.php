a79d5e343c0270819238b698df24ccc75ac43f07
Remygin Ilya <remygin@gmail.com>
2018-12-04 15:15:55 +0300

    123

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'cream-store');
+define('DB_NAME', 'cream');
 
 /** MySQL database username */
 define('DB_USER', 'root');
